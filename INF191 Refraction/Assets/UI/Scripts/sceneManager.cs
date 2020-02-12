0b133bf009e869eaf42ff42ca04455e934898ba9
Zhehao Xie <zhehaox@uci.edu>
2020-02-12 14:47:45 -0800

    Login & Charater Creation UI

---
+++ INF191 Refraction/Assets/UI/Scripts/sceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLoginStage()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadCharaterCreationStage()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainGameStage()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
