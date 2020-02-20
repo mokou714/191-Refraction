65daa874b9e3bec05320d123cbaae9a85ee1f889
Zhehao Xie <zhehaox@uci.edu>
2020-02-19 17:04:24 -0800

    text display and selection

---
+++ INF191 Refraction/Assets/UI/Scripts/SelectField.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectField : MonoBehaviour
+{
+    public string prompt;
+    public Text promptField;
+    public float displaySpeed;
+    public List<Button> selections;
+
+    bool isDisplaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDisplaying && Input.GetMouseButton(0))
+        {
+            StartCoroutine(display());
+        }
+    }
+
+    IEnumerator display()
+    {
+        isDisplaying = true;
+        promptField.text = "";
+        for (int i = 0; i < prompt.Length; ++i)
+        {
+            promptField.text += prompt[i];
+            yield return new WaitForSecondsRealtime(0.1f / displaySpeed);
+        }
+        isDisplaying = false;
+    }
+}
---
+++ INF191 Refraction/Assets/UI/Scripts/TextDisplayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TextDisplayer : MonoBehaviour
+{
+    public float displaySpeed;
+    public Text textField;
+    public string textFilePath;
+
+
+    bool isDisplaying = false;
+    string textContent;
+   
+
+    // Start is called before the first frame update
+    void Start(){
+        StreamReader reader = new StreamReader(textFilePath);
+        textContent = reader.ReadToEnd();
+        reader.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDisplaying && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("!!!!");
+            StartCoroutine(display());
+        }
+    }
+
+    IEnumerator display()
+    {
+        isDisplaying = true;
+        textField.text = "";
+        for(int i = 0; i < textContent.Length; ++i) { 
+            textField.text += textContent[i];
+            yield return new WaitForSecondsRealtime(0.1f / displaySpeed);
+        }
+        isDisplaying = false;
+    }
+}
