adea46cb41d6b847b08cff47c94bd8af9e221cab
Zhehao Xie <zhehaox@uci.edu>
2020-05-30 08:35:07 -0700

    connect4 exit button

---
+++ INF191 Refraction/Assets/Game/Scripts/Connect4/ExitGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    [SerializeField] private sceneManager sceneManager;
+    public void Exit()
+    {
+        if (Data.userData is null || !Data.userData.isEmployer)
+        {
+            sceneManager.LoadMainGameStage();
+        }
+        else
+        {
+            sceneManager.LoadInterviewPortal();
+        }
+    }
+}
